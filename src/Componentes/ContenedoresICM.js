552001c1976e9a58ce70d0cf41aca83c88bf25ba
Artagos <cartayaarnaldo4@gmail.com>
2024-01-26 05:43:13 -0800

    adicion de boton

--- src/Componentes/ContenedoresICM.js
+++ src/Componentes/ContenedoresICM.js
@@ -11,6 +11,7 @@ const ContenedorICM=()=>{
             <p>Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </div>
         </div>
+        <a href="#" class="buttonC">más</a>
     </div>
     <div className='img-side'><img src='event.jpeg' alt='rhcp'></img></div>
 </div>
