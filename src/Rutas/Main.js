8e6d723b546fac72a0b4530e61ddb88bb6f4e409
Artagos <cartayaarnaldo4@gmail.com>
2024-02-07 19:03:03 -0800

    Cnectando front con Back

--- src/Rutas/Main.js
+++ src/Rutas/Main.js
@@ -1,10 +1,27 @@
+import React from "react"
 import CCF from "../Componentes/ContenedorConFondo"
 import ContenedorICM from "../Componentes/ContenedoresICM"
+import axios from 'axios';
 const Main =()=>{
+    const [elements,setElements]=React.useState();
+    React.useEffect(()=>{
+        axios.get('http://localhost:3000/api/entidades/seccion/banner/')
+        .then(response => {
+            console.log(response.data);
+          setElements(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching (Banner Principal)', error);
+        });
+    },
+    [])
+
     return(
         <div>
+        
         <CCF/>
         <ContenedorICM/>
+        
         </div>
     )
 }
