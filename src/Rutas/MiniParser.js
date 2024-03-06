f63638836c5ae1a48d70619836888e735fb09b85
Artagos <cartayaarnaldo4@gmail.com>
2024-03-06 18:24:10 -0500

    url fotos

--- src/Rutas/MiniParser.js
+++ src/Rutas/MiniParser.js
@@ -8,19 +8,19 @@ import CCFST from "../Componentes/ContenedorConFondoST";
 export function MiniParser(data){
     if(data.tipo_contenedor==="ContenedorConFondo"){
         return(
-            <CCF titulo={data.titulo} descripcion={data.descripcion} fondo={data.foto} />
+            <CCF titulo={data.titulo} descripcion={data.descripcion} fondo={'/'+data.foto} />
         )
     }
     else if(data.tipo_contenedor==="ContenedorConFondoSoloTitulo"){
         return(
-            <CCFST fondo={data.foto} titulo={data.titulo}/>
+            <CCFST fondo={'/'+data.foto} titulo={data.titulo}/>
         )
     }
     
     else if(data.tipo_contenedor==="ContenedorICM"){
         return(
             <ContenedorICM titulo={data.titulo} encabezado={data.encabezado} descripcion={data.descripcion} 
-            imagen={data.foto} colorFondo={data.color_de_fondo} colorLetra={data.color_de_letra} 
+            imagen={'/'+data.foto} colorFondo={data.color_de_fondo} colorLetra={data.color_de_letra} 
             colorBoton={data.color_boton} colorLBoton={data.color_letra_boton}/>
         )
     }
--- src/Rutas/Revista.js
+++ src/Rutas/Revista.js
@@ -7,7 +7,7 @@ const Revista =()=>{
         <ContenedorICM titulo={'HISTORIA'} encabezado={'lore ipsum amet'} 
         descripcion={'bbbbdfnsjnhbobbsdhfbabubuybsdhjhbbswd'} colorFondo={'orange'} 
         colorBoton={'white'} colorLboton={'black'} colorLetra={'white'} imagen={'https://cdn.pixabay.com/photo/2024/01/26/23/53/building-8534894_1280.jpg'}/>
-        <CCF fondo={"https://cdn.pixabay.com/photo/2024/01/26/23/53/building-8534894_1280.jpg"} titulo={"Candela"} descripcion={"tengo problemas en el gao"}/>
+        <CCF fondo={"/images/Captura_de_pantalla_2024-02-29_163211_xULR6dW.png"} titulo={"Candela"} descripcion={"tengo problemas en el gao"}/>
         </div>
     )
 }
