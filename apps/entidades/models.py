fc5d1e5b9b48a1930e02e760ed8c816a1571478f
Alejandrito <alejandrito110402@gmail.com>
2024-03-06 16:19:44 -0500

    a

--- apps/entidades/models.py
+++ apps/entidades/models.py
@@ -22,11 +22,11 @@ class ContenedorICM (models.Model):
     color_de_letra = models.CharField(max_length=7)
     color_boton = models.CharField(max_length=7)
     color_letra_boton = models.CharField(max_length=7)
-    
+
 class ContenedorEfemeride (models.Model):
     color_de_fondo = models.CharField(max_length=7)
 
-  
+
 class BannerPrincipal(models.Model):
     CONTENEDOR_CHOICES = [
         ('ContenedorConFondo', 'Contenedor: Titulo + Descripcion + Foto'),
@@ -36,15 +36,15 @@ class BannerPrincipal(models.Model):
         ('Acontecimiento', 'Contenedor: Acontecimiento'),
         ('Efemeride', 'Contenedor: Efemeride'),
     ]
-    
-    
-    
+
+
+
     tipo_contenedor = models.CharField(max_length=50, choices=CONTENEDOR_CHOICES)
     #posicion_en_pantalla = models.PositiveIntegerField(unique=True)
     seleccionar_efemeride = models.ForeignKey('Efemerides', on_delete=models.CASCADE, related_name='banner_principal', null=True)
     seleccionar_acontecimiento = models.ForeignKey('Acontecimiento', on_delete=models.CASCADE, related_name='banner_principal', null=True)
     seleccionar_evento = models.ForeignKey('Evento', on_delete=models.CASCADE, related_name='banner_principal', null=True)
-    
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -54,7 +54,7 @@ class BannerPrincipal(models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -62,19 +62,19 @@ class BannerPrincipal(models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     titulo = models.CharField(max_length=50, null=True)
     encabezado = models.CharField(max_length=50, null=True)
     descripcion = models.CharField(max_length=500, null=True)
     enlace = models.URLField(max_length=200, null=True)
     foto = models.FileField(upload_to='images/', max_length=100, null=True)
-    
-    
+
+
     color_de_fondo = models.CharField(max_length=7, choices=TEXT_COLOR_CHOICES, null=True)
     color_de_letra = models.CharField(max_length=7, choices=TEXT_COLOR_CHOICES, null=True)
     color_boton = models.CharField(max_length=7, choices=TEXT_COLOR_CHOICES, null=True)
     color_letra_boton = models.CharField(max_length=7, choices=TEXT_COLOR_CHOICES, null=True)
-    
+
 
     tipografia_titulo = models.CharField(max_length=50, choices=TEXT_TIPOGRAPHY_CHOICES, null=True)
     tipografia_encabezado = models.CharField(max_length=50, choices=TEXT_TIPOGRAPHY_CHOICES, null=True)
@@ -83,12 +83,29 @@ class BannerPrincipal(models.Model):
 
 
     numero_unico = models.PositiveIntegerField(unique=True, blank=True, null=True)
-    
+
     def __str__(self):
         return self.tipo_contenedor
-    
+
+class Iconos (models.Model):
+    ICON_CHOICES = [
+        ('S_Efemerides', 'Sección Efemerides'),
+        ('S_BannerPpal', 'Sección Banner Principal'),
+        ('S_Eventos', 'Sección Eventos'),
+        ('S_Revista', 'Sección Revista'),
+
+    ]
+
+    seccion = models.CharField(max_length=50, choices=ICON_CHOICES)
+    foto = models.FileField(upload_to='images/', max_length=100, null=True)
+
+    def __str__(self):
+        return self.seccion
+
+
+
 class Seccion_Efemerides (models.Model):
-    
+
     seleccionar_efemeride = models.ForeignKey('Efemerides', on_delete=models.CASCADE, related_name='seccion_efemerides', null=True)
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
@@ -101,10 +118,10 @@ class Seccion_Efemerides (models.Model):
     ]
     color_de_fondo = models.CharField(max_length=7, choices=TEXT_COLOR_CHOICES, null=True)
     numero_unico = models.PositiveIntegerField(unique=True, blank=True, null=True)
-    
+
     def __str__(self):
         return self.seleccionar_efemeride.titulo
-    
+
 
 class Evento (models.Model):
     título = models.CharField(max_length=100, default='None')
@@ -114,8 +131,8 @@ class Evento (models.Model):
     enlace = models.URLField(max_length=200)
     foto = models.FileField(upload_to='images/', max_length=100)
     mostrar_en_banner_principal = models.BooleanField()
-    
-    
+
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -125,7 +142,7 @@ class Evento (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -133,18 +150,18 @@ class Evento (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-    
-    
+
+
     def __str__(self):
         return self.título
-    
+
 
 class Historia_de_la_Institución (models.Model):
     class Meta:
@@ -153,7 +170,7 @@ class Historia_de_la_Institución (models.Model):
     descripción = models.CharField(max_length=500)
     foto = models.FileField(upload_to='images/', max_length=100)
     tipografía = models.CharField(max_length=100)
-    
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -163,7 +180,7 @@ class Historia_de_la_Institución (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -171,16 +188,16 @@ class Historia_de_la_Institución (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-    
-   
-   
+
+
+
     def __str__(self):
         return self.título
 
@@ -191,7 +208,7 @@ class Centros_y_Empresas (models.Model):
     dirección = models.CharField(max_length=500)
     télefono = models.CharField(max_length=20)
     correo = models.EmailField(max_length=100)
-    
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -201,7 +218,7 @@ class Centros_y_Empresas (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -209,17 +226,17 @@ class Centros_y_Empresas (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-    
+
     def __str__(self):
         return self.nombre
-    
+
 class Directores (models.Model):
     class Meta:
         verbose_name_plural = "Directores"
@@ -229,7 +246,7 @@ class Directores (models.Model):
     correo = models.EmailField(max_length=100)
     consejo_de_dirección = models.BooleanField()
     empresa = models.OneToOneField("Centros_y_Empresas", on_delete=models.CASCADE)
-    
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -239,7 +256,7 @@ class Directores (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -247,14 +264,14 @@ class Directores (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
-   
-    
+
+
     def __str__(self):
         return self.nombre
-    
+
 class Premio_Nacional_de_Música (models.Model):
     class Meta:
         verbose_name_plural = "Premio_Nacional_de_Música"
@@ -263,8 +280,8 @@ class Premio_Nacional_de_Música (models.Model):
     descripión = models.CharField(max_length=500)
     bibliografía = models.CharField(max_length=500)
     foto = models.FileField(upload_to='images/', max_length=100)
-    
-    
+
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -274,7 +291,7 @@ class Premio_Nacional_de_Música (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -282,14 +299,14 @@ class Premio_Nacional_de_Música (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     # tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     # color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-   
+
     def __str__(self):
         return self.titulo
 
@@ -301,8 +318,8 @@ class Acontecimiento (models.Model):
     foto = models.FileField(upload_to='images/', max_length=100)
     mostrar_en_banner_principal = models.BooleanField()
     relevante = models.BooleanField()
-    
-    
+
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -312,7 +329,7 @@ class Acontecimiento (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -320,18 +337,18 @@ class Acontecimiento (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-    
-     
+
+
     def __str__(self):
         return self.título
-    
+
 
 class Multimedia (models.Model):
     nombre = models.CharField(max_length=100, default='None')
@@ -339,8 +356,8 @@ class Multimedia (models.Model):
     tipo = models.CharField(max_length=100)
     enlace = models.URLField(max_length=200)
     foto = models.FileField(upload_to='images/', max_length=100)
-    
-    
+
+
     TEXT_COLOR_CHOICES = [
         ('#000000', 'negro'),
         ('#ffffff','blanco'),
@@ -350,7 +367,7 @@ class Multimedia (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -358,17 +375,17 @@ class Multimedia (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
     foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-    
+
     def __str__(self):
         return self.nombre
-    
+
 class Efemerides (models.Model):
     class Meta:
         verbose_name_plural = "Efemérides"
@@ -391,7 +408,7 @@ class Efemerides (models.Model):
         ('0d032b', 'malva'),
         ('ed8500', 'naranja'),
     ]
-    
+
     TEXT_TIPOGRAPHY_CHOICES = [
         ('Bw Darius DEMO Bold', 'Bw Darius DEMO Bold'),
         ('Bw Darius DEMO Regular','Bw Darius DEMO Regular'),
@@ -399,16 +416,14 @@ class Efemerides (models.Model):
         ('Montserrat Regular','Montserrat Regular'),
         ('Montserrat Italic', 'Montserrat Italic'),
     ]
-    
+
     tipografía_de_letra = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Montserrat Medium')
     tipografía_de_título = models.CharField(max_length=100, choices=TEXT_TIPOGRAPHY_CHOICES, default = 'Bw Darius DEMO Bold')
     color_de_letra = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_título = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#000000')
     color_de_fondo = models.CharField(max_length=7,choices=TEXT_COLOR_CHOICES, default='#ffffff')
    # foto_de_fondo = models.FileField(upload_to='images/', max_length=100)
-   
-   
+
+
     def __str__(self):
         return self.titulo
-   
-   
