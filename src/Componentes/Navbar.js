29286caf1ba6e37dff8df9030652229ecc1d62d3
Artagos <cartayaarnaldo4@gmail.com>
2024-03-07 10:50:24 -0500

    ya esta weno ya

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -17,7 +17,6 @@ function Navbar({ seccion }) {
   React.useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/entidades/logos')
       .then(response => {
-        console.log(response.data)
         var icons = response.data.iconos
         icons.map((icon) =>{ 
 
