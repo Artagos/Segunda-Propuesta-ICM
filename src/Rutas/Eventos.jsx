2c7abad4c7906fbf63c1b12fceffcb5c789f0983
Artagos <105867005+Artagos@users.noreply.github.com>
2024-04-09 23:39:31 -0400

    Update Eventos.jsx

--- src/Rutas/Eventos.jsx
+++ src/Rutas/Eventos.jsx
@@ -1,6 +1,7 @@
 import ContenedorEventos from "../Componentes/ContenedorEventos";
 import Navbar from "../Componentes/Navbar";
 import React from "react";
+import http from "./axiosAux";
 const Eventos=()=>{
     const [eventos,setEventos]=React.useState([]);
     React.useEffect(()=>{
