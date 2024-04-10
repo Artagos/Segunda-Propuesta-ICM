3656315e9a6f3ef5200d2a1eceee3dccdba0d652
Artagos <105867005+Artagos@users.noreply.github.com>
2024-04-09 21:23:03 -0400

    Update axiosAux.js

--- src/Rutas/axiosAux.js
+++ src/Rutas/axiosAux.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://dcubamusica.cult.cu/api/' : '';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://dcubamusica.cult.cu/api/' : 'https://localhost:8000';
 
 const http = axios.create({
     baseURL: baseURL,
