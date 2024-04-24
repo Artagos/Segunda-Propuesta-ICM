f97f382c70c9da9a0beca7a70c030da61ff9622a
Alejandrito <alejandrito110402@gmail.com>
2024-04-24 17:49:03 -0400

    update

--- apps/entidades/views.py
+++ apps/entidades/views.py
@@ -5,13 +5,25 @@ from django.core.exceptions import ObjectDoesNotExist
 from datetime import datetime, timedelta
 from calendar import monthrange
 from django.http import JsonResponse
-from .models import Efemerides, Acontecimiento, Evento, Centros_y_Empresas, Directores, Historia_de_la_Institución, Multimedia, Premio_Nacional_de_Música, Iconos, Revista, BannerPrincipal, Seccion_Efemerides, Podcast
+from .models import Efemerides, Acontecimiento, Evento, Centros_y_Empresas, Directores, Historia_de_la_Institución, Multimedia, Premio_Nacional_de_Música, Iconos, Revista, BannerPrincipal, Podcast
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
+from django.db.models.functions import ExtractMonth, ExtractDay, ExtractYear
 
 def lista_revistas(request):
-    revistas = Revista.objects.all().values('id', 'titulo', 'descripcion', 'pdf', 'imagen_portada')
-    return JsonResponse(list(revistas), safe=False)
+    revistas = Revista.objects.all()
+    contenedores_list = []
+
+    for contenedor in revistas:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'descripcion': contenedor.descripcion,
+            'foto': contenedor.get_foto_url(),
+            'pdf':contenedor.get_pdf_url(),
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
 
 def detalle_revista(request, id):
     revista = get_object_or_404(Revista, pk=id)
@@ -24,29 +36,41 @@ def detalle_revista(request, id):
     return JsonResponse(data)
 
 def lista_podcasts(request):
-    podcasts = Podcast.objects.all().values('id', 'titulo', 'descripcion', 'link_podcast', 'foto')
+    podcasts = Podcast.objects.all()
     podcasts_data = []
     for podcast in podcasts:
         podcast_data = {
-            'titulo': podcast['titulo'],
-            'descripcion': podcast['descripcion'],
-            'link_podcast': podcast['link_podcast'],
-            'foto_url': request.build_absolute_uri(podcast['foto']),  # Construye la URL completa para la foto
-            'es_link_local': Podcast.objects.get(pk=podcast['id']).es_link_local(),
+            'id': podcast.id,
+            'titulo': podcast.titulo,
+            'descripcion': podcast.descripcion,
+            'link_podcast': podcast.link_podcast,
+            'foto': podcast.get_foto_url(),
         }
         podcasts_data.append(podcast_data)
     return JsonResponse(podcasts_data, safe=False)
 
 def get_banner_principal(request):
-    contenedores = BannerPrincipal.objects.all().order_by('numero_unico').values()
-    return JsonResponse(list(contenedores), safe=False)
+    contenedores = BannerPrincipal.objects.all().order_by('numero_unico')
+    contenedores_list = []
 
-def get_seccion_efemerides(request):
-    contenedores = Seccion_Efemerides.objects.all().order_by('numero_unico').values()
-    return JsonResponse(list(contenedores), safe=False)
+    for contenedor in contenedores:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'tipo_contenedor': contenedor.tipo_contenedor,
+            'seleccionar_efemeride':contenedor.seleccionar_efemeride,
+            'seleccionar_acontecimiento':contenedor.seleccionar_acontecimiento,
+            'seleccionar_evento':contenedor.seleccionar_evento,
+            'titulo': contenedor.titulo,
+            'descripcion': contenedor.descripcion,
+            'foto': contenedor.get_foto_url(),
+            'color_de_fondo':contenedor.color_de_fondo,
+            'numero_unico':contenedor.numero_unico,
+            'encabezado':contenedor.encabezado,
 
+            # Agrega aquí cualquier otro campo que quieras incluir en la respuesta
+        })
 
-
+    return JsonResponse(contenedores_list, safe=False)
 
 def get_iconos(request):
 
@@ -61,7 +85,7 @@ def get_iconos(request):
     iconos_data = [
         {
             'seccion': icono.get_seccion_display(),
-            'foto': request.build_absolute_uri(icono.foto.url) if icono.foto else None
+            'foto': icono.get_foto_url(),
         }
         for icono in iconos_seleccionados
     ]
@@ -71,89 +95,198 @@ def get_iconos(request):
 
 
 def get_all_efem(request):
-    efemerides = Efemerides.objects.all().order_by().values()
-    return JsonResponse(list(efemerides), safe=False)
+    contenedores = Efemerides.objects.all().order_by().values()
+    contenedores_list = []
 
-def get_efem_premio(request):
-    efemerides = Efemerides.objects.get(premio_nacional_de_la_música is not None).values()
-    return JsonResponse(list(efemerides), safe=False)
+    for contenedor in contenedores:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'fecha': contenedor.fecha,
+            'descripcion': contenedor.descripcion,
+            'foto': contenedor.get_foto_url(),
+            'color_de_fondo':contenedor.color_de_fondo,
+            'encabezado':contenedor.encabezado,
 
-def get_efem_a_mostrar(request):
-    efemerides = Efemerides.objects.get(mostrar_en_banner_principal = true).values()
-    return JsonResponse(list(efemerides), safe=False)
+        })
 
-def get_acontecimiento_relevante(request):
-    acontecimiento = Acontecimiento.objects.get(relevante = true).values()
-    return JsonResponse(list(acontecimiento), safe=False)
+    return JsonResponse(contenedores_list, safe=False)
 
 
-def get_acontecimiento_a_mostrar(request):
-    acontecimiento = Acontecimiento.objects.get(mostrar_en_banner_principal = true).values()
-    return JsonResponse(list(acontecimiento), safe=False)
 
-def get_evento_a_mostrar(request):
-    evento = Evento.objects.get(mostrar_en_banner_principal = true).values()
-    return JsonResponse(list(evento), safe=False)
+def get_efem_by_date(request, day, month):
+    contenedores = Efemerides.objects.filter(fecha__day=day, fecha__month=month).order_by('fecha').values()
+    contenedores_list = []
+
+    for contenedor in contenedores:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'fecha': contenedor.fecha,
+            'descripcion': contenedor.descripcion,
+            'foto': contenedor.get_foto_url(),
+            'color_de_fondo':contenedor.color_de_fondo,
+            'encabezado':contenedor.encabezado,
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
+
+
+
+def get_acontecimiento_by_date(request, month, year):
+    acontecimientos = Acontecimiento.objects.filter(fecha__month=month, fecha__year=year).order_by('fecha').values()
+    acontecimientos_list = []
+
+    for acontecimiento in acontecimientos:
+        acontecimientos_list.append({
+            'id': acontecimiento.id,
+            'titulo': acontecimiento.titulo,
+            'fecha': acontecimiento.fecha,
+            'descripcion': acontecimiento.descripcion,
+            'foto': acontecimiento.get_foto_url(),
+            'color_de_fondo': acontecimiento.color_de_fondo,
+            'encabezado': acontecimiento.encabezado,
+        })
+
+    return JsonResponse(acontecimientos_list, safe=False)
+
+def get_evento_by_date(request, month, year):
+    eventos = Evento.objects.filter(fecha__month=month, fecha__year=year).order_by('fecha').values()
+    eventos_list = []
+
+    for evento in eventos:
+        eventos_list.append({
+            'id': evento.id,
+            'titulo': evento.titulo,
+            'fecha': evento.fecha,
+            'descripcion': evento.descripcion,
+            'foto': evento.get_foto_url(),
+            'color_de_fondo': evento.color_de_fondo,
+            'encabezado': evento.encabezado,
+            'hora': evento.hora,
+        })
+
+    return JsonResponse(eventos_list, safe=False)
+
+
 
 def get_historia(request):
     historia = Historia.objects.all().values()
-    return JsonResponse(list(historia), safe=False)
+    contenedores_list = []
+
+    for contenedor in historia:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'descripcion': contenedor.descripcion,
+            'foto': contenedor.get_foto_url(),
+            'color_de_fondo':contenedor.color_de_fondo,
+
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
 
 def get_centros_y_directores(request):
     centros_y_directores = Directores.objects.select_related('empresa').all().values()
-    return JsonResponse(list(centros_y_directores), safe=False)
+    contenedores_list = []
+
+    for contenedor in centros_y_directores:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'cargo': contenedor.cargo,
+            'telefono': contenedor.télefono,
+            'foto': contenedor.get_foto_url(),
+            'consejo_de_direccion': contenedor.consejo_de_dirección,
+            'color_de_fondo':contenedor.color_de_fondo,
+            'empresa': str(contenedor.empresa.nombre),
+
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
 
 def get_premios(request):
     premios = Premio_Nacional_de_Música.objects.order_by('fecha').values()
-    return JsonResponse(list(premios), safe=False)
+    contenedores_list = []
+
+    for contenedor in premios:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'descripcion': contenedor.descripcion,
+            'bibliografia': contenedor.bibliografía,
+            'foto': contenedor.get_foto_url(),
+            'color_de_fondo':contenedor.color_de_fondo,
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
 
 def get_acontecimientos_semana(request):
     fecha = datetime.now()
     fecha_inicio = fecha - timedelta(days = fecha.weekday())
     fecha_fin = fecha_inicio + timedelta(days = 6)
-    acontecimientos_semana = Acontecimiento.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).values()
-    return JsonResponse(list(acontecimientos_semana), safe=False)
+    acontecimientos_semana = Acontecimiento.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).order_by('fecha').values()
+    acontecimientos_list = []
+
+    for acontecimiento in acontecimientos_semana:
+        acontecimientos_list.append({
+            'id': acontecimiento.id,
+            'titulo': acontecimiento.titulo,
+            'fecha': acontecimiento.fecha,
+            'descripcion': acontecimiento.descripcion,
+            'foto': acontecimiento.get_foto_url(),
+            'color_de_fondo': acontecimiento.color_de_fondo,
+            'encabezado': acontecimiento.encabezado,
+        })
+
+    return JsonResponse(acontecimientos_list, safe=False)
 
 def get_eventos_semana(request):
     fecha = datetime.now()
     fecha_inicio = fecha - timedelta(days = fecha.weekday())
     fecha_fin = fecha_inicio + timedelta(days = 6)
-    eventos_semana = Evento.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).values()
-    return JsonResponse(list(eventos_semana), safe=False)
+    eventos_semana = Evento.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).order_by('fecha').values()
+    eventos_list = []
 
-def get_acontecimientos_mes(request):
-    fecha = datetime.now()
-    fecha_inicio = fecha.replace(day = 1)
-    fecha_fin = fecha_inici0.replace(day= monthrange(fecha.year, fecha.month)[1])
-    acontecimientos_mes = Acontecimiento.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).values()
-    return JsonResponse(list(acontecimientos_mes), safe=False)
+    for evento in eventos_semana:
+        eventos_list.append({
+            'id': evento.id,
+            'titulo': evento.titulo,
+            'fecha': evento.fecha,
+            'descripcion': evento.descripcion,
+            'foto': evento.get_foto_url(),
+            'color_de_fondo': evento.color_de_fondo,
+            'encabezado': evento.encabezado,
+            'hora': evento.hora,
+        })
 
-def get_eventos_mes(request):
-    fecha = datetime.now()
-    fecha_inicio = fecha.replace(day = 1)
-    fecha_fin = fecha_inici0.replace(day= monthrange(fecha.year, fecha.month)[1])
-    eventos_mes = Eventos.objects.filter(fecha__range = [fecha_inicio, fecha_fin]).values()
-    return JsonResponse(list(eventos_mes), safe=False)
+    return JsonResponse(eventos_list, safe=False)
 
 def get_centros_contactos(request):
     centros = Centros_y_Empresas.objects.all().values()
-    data = [
-        {
-            'nombre': centro.nombre,
-            'telefono': centro.teléfono,
-            'email': centro.correo,
-        }
-        for centro in centros
-    ]
-    return JsonResponse(data, safe=False)
+    contenedores_list = []
 
-def get_directores_nombres(request):
-    directores = Directores.objects.all().values()
-    data = [
-        {
-            'nombre': director.nombre,
+    for contenedor in centros:
+        contenedores_list.append({
+            'id': contenedor.id,
+            'titulo': contenedor.titulo,
+            'telefono': contenedor.télefono,
+            'foto': contenedor.get_foto_url(),
+            'direccion': contenedor.dirección,
+            'correo':contenedor.correo,
 
-        }
-        for director in directores
-    ]
-    return JsonResponse(data, safe=False)
+
+        })
+
+    return JsonResponse(contenedores_list, safe=False)
+
+# def get_directores_nombres(request):
+#     directores = Directores.objects.all().values()
+#     data = [
+#         {
+#             'nombre': director.nombre,
+
+#         }
+#         for director in directores
+#     ]
+#     return JsonResponse(data, safe=False)
